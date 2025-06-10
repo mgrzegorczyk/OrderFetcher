546f444c24a728b0630ac4b010c8df418f016e62
mgrzegor <m-grzegorczyk@o2.pl>
2025-06-10 18:01:38 +0200

    entities

---
+++ src/OrderFetcher.Domain/Entities/Order.cs
@@ -0,0 +1,20 @@
+﻿namespace OrderFetcher.Domain.Entities;
+
+public class Order
+{
+    public int Id { get; set; }
+    public int OrderNumber { get; set; }
+    public DateTime OrderDate { get; set; }
+    public decimal Amount { get; set; }
+    public decimal TotalAmount { get; set; }
+    public string Currency { get; set; }
+    public string ShippingMethod { get; set; }
+    public string PaymentMethod { get; set; }
+    public ICollection<OrderItem> Items { get; set; }
+    
+    public int BillingAddressId { get; set; }
+    public Address BillingAddress { get; set; }
+
+    public int? ShippingAddressId { get; set; }
+    public Address? ShippingAddress { get; set; }
+}
---
+++ src/OrderFetcher.Domain/Entities/OrderItem.cs
@@ -0,0 +1,12 @@
+﻿namespace OrderFetcher.Domain.Entities;
+
+public class OrderItem
+{
+    public int Id { get; set; }
+    public string ProductName { get; set; }
+    public int Quantity { get; set; }
+    public decimal Price { get; set; }
+    
+    public int OrderId { get; set; }
+    public Order Order { get; set; }
+}
