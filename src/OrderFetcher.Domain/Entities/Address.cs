905399bdb3e188432d4504009bce80f6f8c42b14
mgrzegor <m-grzegorczyk@o2.pl>
2025-06-11 22:52:06 +0200

    Address entity - optional phone and email

--- src/OrderFetcher.Domain/Entities/Address.cs
+++ src/OrderFetcher.Domain/Entities/Address.cs
@@ -8,6 +8,6 @@ public class Address
     public string BuildingNumber { get; set; } 
     public string PostalCode { get; set; }
     public string City { get; set; } 
-    public string PhoneNumber { get; set; }
-    public string Email { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? Email { get; set; }
 }
