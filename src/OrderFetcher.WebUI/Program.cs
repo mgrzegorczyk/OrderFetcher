1b9e44d893a2dea4bdbd1074a66c41421fdf7065
mgrzegorczyk <mgrzegorczyk@betacom.com.pl>
2025-06-12 02:12:41 +0200

    app start migrator

--- src/OrderFetcher.WebUI/Program.cs
+++ src/OrderFetcher.WebUI/Program.cs
@@ -24,6 +24,12 @@ builder.Services.AddScoped<IOrderGPTMapper, OrderGPTMapper>();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
